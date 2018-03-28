54128ef54c28204f0ca4d50145115306937a3552
madeleinea <maddy1295@gmail.com>
2018-03-28 13:24:29 -0700

    run this to enable GPU

---
+++ CUDA/mexGPUall.m
@@ -0,0 +1,13 @@
+% mexGPUall. For these to complete succesfully, you need to configure the
+% Matlab GPU library first (see README files for platform-specific
+% information)
+if ispc
+    mexcuda -largeArrayDims mexMPmuFEAT.cu
+    mexcuda -largeArrayDims mexMPregMU.cu
+    mexcuda -largeArrayDims mexWtW2.cu
+else
+    mex -largeArrayDims mexMPmuFEAT.cu
+    mex -largeArrayDims mexMPregMU.cu
+    mex -largeArrayDims mexWtW2.cu
+end
+
